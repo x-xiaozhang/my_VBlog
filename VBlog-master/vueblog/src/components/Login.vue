bb74b6142abebcc952b39eef36bf48df171e820d
xq945 <3059697831@qq.com>
2025-11-26 20:09:15 +0800

    增加logo

--- VBlog-master/vueblog/src/components/Login.vue
+++ VBlog-master/vueblog/src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <el-form :rules="rules" class="login-container" label-position="left"
            label-width="0px" v-loading="loading">
+    <!-- 修改为图片路径 -->
+    <div class="login_logo">
+      <img src="../assets/logo.jpg" alt="VBlog系统logo" class="logo-img">
+    </div>
     <h3 class="login_title">系统登录</h3>
     <el-form-item prop="account">
       <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
@@ -15,72 +19,86 @@
   </el-form>
 </template>
 <script>
-  import {postRequest} from '../utils/api'
-  import {putRequest} from '../utils/api'
-  export default{
-    data(){
-      return {
-        rules: {
-          account: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-          checkPass: [{required: true, message: '请输入密码', trigger: 'blur'}]
-        },
-        checked: true,
-        loginForm: {
-          username: 'sang',
-          password: '123'
-        },
-        loading: false
-      }
-    },
-    methods: {
-      submitClick: function () {
-        var _this = this;
-        this.loading = true;
-        postRequest('/login', {
-          username: this.loginForm.username,
-          password: this.loginForm.password
-        }).then(resp=> {
-          _this.loading = false;
-          if (resp.status == 200) {
-            //成功
-            var json = resp.data;
-            if (json.status == 'success') {
-              _this.$router.replace({path: '/home'});
-            } else {
-              _this.$alert('登录失败!', '失败!');
-            }
+import {postRequest} from '../utils/api'
+import {putRequest} from '../utils/api'
+export default{
+  data(){
+    return {
+      rules: {
+        account: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+        checkPass: [{required: true, message: '请输入密码', trigger: 'blur'}]
+      },
+      checked: true,
+      loginForm: {
+        username: 'sang',
+        password: '123'
+      },
+      loading: false
+    }
+  },
+  methods: {
+    submitClick: function () {
+      var _this = this;
+      this.loading = true;
+      postRequest('/login', {
+        username: this.loginForm.username,
+        password: this.loginForm.password
+      }).then(resp=> {
+        _this.loading = false;
+        if (resp.status == 200) {
+          //成功
+          var json = resp.data;
+          if (json.status == 'success') {
+            _this.$router.replace({path: '/home'});
           } else {
-            //失败
             _this.$alert('登录失败!', '失败!');
           }
-        }, resp=> {
-          _this.loading = false;
-          _this.$alert('找不到服务器⊙﹏⊙∥!', '失败!');
-        });
-      }
+        } else {
+          //失败
+          _this.$alert('登录失败!', '失败!');
+        }
+      }, resp=> {
+        _this.loading = false;
+        _this.$alert('找不到服务器⊙﹏⊙∥!', '失败!');
+      });
     }
   }
+}
 </script>
 <style>
-  .login-container {
-    border-radius: 15px;
-    background-clip: padding-box;
-    margin: 180px auto;
-    width: 350px;
-    padding: 35px 35px 15px 35px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-  }
+.login-container {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 150px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+  text-align: center;
+}
 
-  .login_title {
-    margin: 0px auto 40px auto;
-    text-align: center;
-    color: #505458;
-  }
+.login_logo {
+  margin: 0 auto 20px;
+  width: 80px;
+  height: 80px;
+}
 
-  .login_remember {
-    margin: 0px 0px 35px 0px;
-    text-align: left;
-  }
+/* 控制图片显示样式 */
+.logo-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain; /* 保持图片比例，完整显示在容器内 */
+}
+
+.login_title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+
+.login_remember {
+  margin: 0px 0px 35px 0px;
+  text-align: left;
+}
 </style>
